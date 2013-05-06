6e61f5ab764dd156f21111e491d1c5a37ccb7f2e
Gregory Jansen <count0@email.unc.edu>
2013-05-06 12:35:39 -0400

    initial commit of JRE bundling changes for mac
    added juno target
    
    e4/juno target changes
    
    added assembly that builds a Mac distribution as an App with bundled Java
    
    moved all mac osx launcher and jre config into the eclipse repository project
    
    edit to product information
    added scm url to bundle manifests

--- crosswalk-gmf.diagram/src/crosswalk/diagram/edit/policies/InputOutputItemSemanticEditPolicy.java
+++ crosswalk-gmf.diagram/src/crosswalk/diagram/edit/policies/InputOutputItemSemanticEditPolicy.java
@@ -15,6 +15,7 @@
  */
 package crosswalk.diagram.edit.policies;
 
+import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
